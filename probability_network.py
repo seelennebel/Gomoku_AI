710aba5895bebbae3af67d34a0b10b962b2a6d45
seelennebel <mr.andrey.amitan@gmail.com>
2024-08-31 02:59:40 +0300

    advancements

--- probability_network.py
+++ probability_network.py
@@ -27,19 +27,49 @@ def generate_initial_state(first_turn):
     if first_turn:
         return board
     else:
-        # this one doesn't work
-        return board[np.random.randint(16), np.random.randint(16)]
+        x = np.random.randint(0, 15, dtype=int)
+        y = np.random.randint(0, 15, dtype=int) 
+        board[x, y] = -1
+        return board
 
-print(generate_initial_state(first_turn = True))
+inittial_state = generate_initial_state(first_turn = False)
 
 class Network:
 
     def __init__(self, initial_state):
         self.initial_state = initial_state
         self.initial_node = Node(initial_state, None, None, 1, None, 0)
+        self.current_node = initial_node
 
     def expand_node(self, node):
         return
 
+    def is_winner(board, move):
+        i, j = move
+        player = board[i, j]
+
+        def check(values):
+            counter = 0
+            for value in values:
+                if value == player:
+                    counter += 1
+                else:
+                    counter = 0
+                if counter >= WIN_LINE_LENGTH:
+                    return True
+            return False
+
+        c1 = check(board[i, :])
+        c2 = check(board[:, j])
+        c3 = check(board.diagonal(j-i))
+        c4 = check(np.fliplr(board).diagonal(board.shape[0]-i-j-1))
+        
+        if c1 or c2 or c3 or c4:
+            return player
+        
+        return False
+
     def run(self):
-        return
+        if current_node.expanded == False:
+            current_node.expand()
+
