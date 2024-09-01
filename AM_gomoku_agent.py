93b5cebdceb6fb729f10e5fb299023db78895fcd
seelennebel <mr.andrey.amitan@gmail.com>
2024-09-01 16:41:19 +0200

    updated gomoku agent

--- AM_gomoku_agent.py
+++ AM_gomoku_agent.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-#either create an empty numpy array or randomly put single enemy point
 def generate_initial_state(first_turn):
 
     board = np.zeros((15, 15))
@@ -19,31 +18,24 @@ class Node:
         self.state = state
         self.expanded = False
         self.nodes_to_expand = []
+        self.children = []
 
     def possible_moves(self):
-        return Node.__generate_possible_expansions(self, self.state)
+        return self.generate_possible_moves()
 
     def calculate_UCB(self):
         return self.prior_probability * math.sqrt(math.log(self.parent_node_simulations) / self.node_simulations) 
 
-    def __generate_possible_moves(self, state):
+    def generate_possible_moves(self):
         possible_expansions = [] 
         for row in range(15):
             for index in range(15):
-                if state[row, index] == 0:
+                if self.state[row, index] == 0:
                     possible_expansions.append((row, index)) 
         return possible_expansions
 
     def __str__(self):
-        return """
-                Initial state:\n{}\n
-                Parent:\n{}\n
-                Children:\n{}\n
-                Prior probability:\n{}\n
-                Parent node simulations:\n{}\n
-                Node simulations:\n{}\n
-                Node expanded:\n{}\n
-                """.format(self.state, self.parent, self.expansions, self.prior_probability, self.parent_node_simulations, self.node_simulations, self.expanded)
+        return "Node: {}".format(self.state)
 
 class GomokuAgent:
 
@@ -56,20 +48,61 @@ class GomokuAgent:
         root_node = Node(board)
         current_node = root_node
         if not current_node.expanded:
-            for move in current_node.possible_moves()):
-                self.expand_node(current_node, move, board)
 
-    def expand_node(self, node, move, board):
+            for move in current_node.possible_moves():
+                self.expand_agent_node(current_node, move, board.copy())
+
+            for node in current_node.nodes_to_expand:
+                current_node.children.insert(-1, node)
+                self.simulate(node, agent_turn=False)
+
+    def expand_agent_node(self, node, move, board):
         board[move[0], move[1]] = self.agent_symbol
-        node.nodes_to_expand.append(board)
+        new_node = Node(board)
+        node.nodes_to_expand.insert(-1, new_node)
 
-    def simulate(self):
-        return
+    def backpropagate(self)
+        
+    def simulate(self, node, agent_turn = False):
+
+        if not agent_turn:
+
+            possible_moves = node.generate_possible_moves()
+
+            for move in possible_moves:
+
+                random_move_index = np.random.randint(0,len(possible_moves))
+                node.state[move[0], move[1]] = self.opponent_symbol
+
+                if self.is_winner(node.state, move):
+                    self.backpropagate()
+
+    def is_winner(board, move):
+        i, j = move
+        player = board[i, j]
+
+        def check(values):
+            counter = 0
+            for value in values:
+                if value == player:
+                    counter += 1
+                else:
+                    counter = 0
+                if counter >= WIN_LINE_LENGTH:
+                    return True
+            return False
+
+        c1 = check(board[i, :])
+        c2 = check(board[:, j])
+        c3 = check(board.diagonal(j-i))
+        c4 = check(np.fliplr(board).diagonal(board.shape[0]-i-j-1))
+        
+        if c1 or c2 or c3 or c4:
+            return player
+        
+        return False
 
 initial_state = generate_initial_state(first_turn=False)
 
-net = ProbabilityNetwork(initial_state)
-net.run()
-
-
-
+net = GomokuAgent(1, 0, -1)
+net.play(initial_state)
